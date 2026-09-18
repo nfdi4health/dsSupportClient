c2d65747b677700e65dfa2bdc29a65305bbfdb85
FlorianSchw <Florian.Schwarz@dife.de>
2026-09-18 22:51:06 +0200

    ci: testing behaviour of workflows

---
+++ R/ds.temp.test.R
@@ -0,0 +1,16 @@
+#' DataSHIELD helper function for histograms
+#'
+#' @param datasources An Opal connection
+#' @param method Type of method for building the histogram
+#' @return It creates a directory with subdirectories of each study, containing histograms of all variables
+#' @export
+#' @importFrom grDevices dev.off png
+#' @import DSI
+#' @import dsBaseClient
+#' @import methods
+#'
+
+ds.temp.test<- function(datasources = NULL, method = "deterministic" ) {
+
+
+}
